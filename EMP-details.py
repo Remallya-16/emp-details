0faf513829449b205130157988070646f16e9a1a
Remallya-16 <kremallya9@gmail.com>
2025-11-19 19:15:41 +0530

    Create EMP-details.py

---
+++ EMP-details.py
@@ -0,0 +1,8 @@
+emp_name=Remallya
+emp_age=35
+emp_salary=100000
+experience=3
+print("Employee Name:",emp_name)
+print("Employee Age:",emp_age)
+print("Employee Salary:",emp_salary)
+print("Employee Experience:",experience)
